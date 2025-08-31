4e27e95c43326e89ec00624a82524e709a7fb8e6
karthiking660-cmd <karthiking660@gmail.com>
2025-08-31 16:41:36 +0530

    Add files via upload
    this is my first project

---
+++ video & audio/script.js
@@ -0,0 +1,16 @@
+function playvideo(){
+video=document.getElementById("video");
+video.play();
+}
+function pausevideo(){
+video=document.getElementById("video");
+video.pause();
+}
+function playaudio(){
+audio=document.getElementById("audio");
+audio.play();
+}
+function pauseaudio(){
+audio=document.getElementById("audio");
+audio.pause();
+}
